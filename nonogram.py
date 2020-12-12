817a8e256f94b339b5f94203448bb4cdd93b518c
zoharmilul <8qxddflhxbw>
2020-12-12 11:24:26 +0200

    new

--- nonogram.py
+++ nonogram.py
@@ -4,30 +4,31 @@ EMPTY = 0
 UNKNOWN = -1
 
 def constraint_satisfactions (n, blocks):
-    options_list = []
-    options_list += _constraint_helper(n,blocks,[0] * n,[])
+    options_list = _constraint_helper(n,blocks,[0] * n,[])
     return options_list
 
+
 def _constraint_helper(n, blocks , row, options_list):
-    if blocks == []:
-        options_list.append(row)
+    if not blocks:
+        options_list.append(row[:])
         return row
-    if n == 0:
-        return
     value = blocks[0]
     for ind in range(n):
         if _can_fill(row, value, ind):
             row = _fill_row(row, value, ind)
             _constraint_helper(n, blocks[1:], row, options_list)
-        else:
-            continue
+            row = _unfill_row(row, value, ind)
     return options_list
 
 
 def _can_fill(row,value,ind):
     return ind <= len(row)-value and row[ind] == 0 and row[ind-1] != 1
 
-
+def _unfill_row(row,value, ind):
+    for num in range(value):
+        row[ind] = EMPTY
+        ind += 1
+    return row
 
 def _fill_row(row, value, ind):
     if value > len(row):
--- test_nonogram.py
+++ test_nonogram.py
@@ -6,3 +6,4 @@ def test_can_fill():
     assert nonogram._can_fill([0, 0, 0, 0, 0], 7, 0) == False
     assert nonogram._can_fill([1, 1, 1, 0, 0], 1, 4) == True
     assert nonogram._can_fill([1,1,1,1,0], 2, 0) == False
+    assert nonogram._can_fill([1, 1, 0, 0, 0], 1, 2) == False
