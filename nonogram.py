d165553323ebbf131d6e3dbca685904ae4a623d5
zoharmilul <8qxddflhxbw>
2020-12-12 12:21:54 +0200

    new

--- nonogram.py
+++ nonogram.py
@@ -4,31 +4,33 @@ EMPTY = 0
 UNKNOWN = -1
 
 def constraint_satisfactions (n, blocks):
-    options_list = _constraint_helper(n,blocks,[0] * n,[])
+    options_list = _constraint_helper(n, blocks, [0] * n, [], 0)
     return options_list
 
 
-def _constraint_helper(n, blocks , row, options_list):
+def _constraint_helper(n, blocks, row, options_list, ind):
     if not blocks:
         options_list.append(row[:])
-        return row
+        return
     value = blocks[0]
-    for ind in range(n):
-        if _can_fill(row, value, ind):
-            row = _fill_row(row, value, ind)
-            _constraint_helper(n, blocks[1:], row, options_list)
-            row = _unfill_row(row, value, ind)
+    for num in range(ind, n):
+        if _can_fill(row, value, num):
+            row = _fill_row(row, value, num)
+            _constraint_helper(n, blocks[1:], row, options_list, num)
+            row = _unfill_row(row, value, num)
     return options_list
 
 
-def _can_fill(row,value,ind):
-    return ind <= len(row)-value and row[ind] == 0 and row[ind-1] != 1
+def _can_fill(row , value , ind):
+    return ind <= len(row) - value and row[ind] == 0 and row[ind - 1] != 1
 
-def _unfill_row(row,value, ind):
+
+def _unfill_row(row, value, ind):
     for num in range(value):
         row[ind] = EMPTY
         ind += 1
     return row
+
 
 def _fill_row(row, value, ind):
     if value > len(row):
@@ -39,6 +41,8 @@ def _fill_row(row, value, ind):
         row[ind] = FILLED
         ind += 1
     return row
-
+print(constraint_satisfactions(3, [1]))
+print(constraint_satisfactions(3, [2]))
+print(constraint_satisfactions(3, [1,1]))
+print(constraint_satisfactions(4, [1,1]))
 print(constraint_satisfactions(5, [2,1]))
-print(constraint_satisfactions(10, [2,1]))
