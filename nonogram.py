cd9b26abce7ddb9ec9d826340962cb51f7c4235a
zoharmilul <8qxddflhxbw>
2020-12-14 13:56:16 +0200

    new

--- nonogram.py
+++ nonogram.py
@@ -22,7 +22,7 @@ def _constraint_helper(n, blocks, row, options_list, ind):
 
 
 def _can_fill(row , value , ind):
-    return ind <= len(row) - value and row[ind] == 0 and row[ind - 1] != 1
+    return ind <= len(row) - value and (row[ind] == EMPTY or row[ind] == UNKNOWN) and row[ind - 1] != FILLED
 
 def _fill_row(row, value, ind, action = FILLED):
     if value > len(row):
@@ -34,5 +34,34 @@ def _fill_row(row, value, ind, action = FILLED):
         ind += 1
     return row
 
+def _check_sum(row):
+    sum = 0
+    for i in row:
+        if i == 1:
+            sum += 1
+    return sum
+
 def row_variations(row, blocks):
     pass
+
+def _row_variations_helper(row, blocks, ind, sum ,lst):
+    if _check_sum(row) == sum:
+        lst.append(row[:])
+        return row
+
+    for index in range(ind, len(row)):
+        for value in range(blocks[0]):
+            if row[index] == UNKNOWN:
+                row[index] = FILLED
+                _row_variations_helper(row, blocks, index, sum, lst)
+                row[index] = UNKNOWN
+            if row[index] == EMPTY:
+                return
+            if row[index] == FILLED:
+                _row_variations_helper(row, blocks, index, sum - 1, lst)
+
+"""print(row_variations([1, 1, -1, 0], [3]))
+print(row_variations([-1, -1, -1, 0], [2]))
+print(row_variations([-1,0,1,0,-1],[1,1]))"""
+
+
