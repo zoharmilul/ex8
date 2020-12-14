7a75ea9847b5886d6a345086792ca29ae130dfa4
zoharmilul <8qxddflhxbw>
2020-12-14 17:40:54 +0200

    new

--- nonogram.py
+++ nonogram.py
@@ -3,8 +3,11 @@ FILLED = 1
 EMPTY = 0
 UNKNOWN = -1
 
+
 def constraint_satisfactions (n, blocks):
-    options_list = _constraint_helper(n, blocks, [0] * n, [], 0)
+    options_list = _constraint_helper(n, blocks, [UNKNOWN] * n, [], 0)
+    for option in options_list:
+        change_unknown_to_empty(option)
     return options_list
 
 
@@ -21,10 +24,11 @@ def _constraint_helper(n, blocks, row, options_list, ind):
     return options_list
 
 
-def _can_fill(row , value , ind):
+def _can_fill(row, value, ind):
     return ind <= len(row) - value and (row[ind] == EMPTY or row[ind] == UNKNOWN) and row[ind - 1] != FILLED
 
-def _fill_row(row, value, ind, action = FILLED):
+
+def _fill_row(row, value, ind, action=FILLED):
     if value > len(row):
         return row
     if ind > len(row) - value:
@@ -35,117 +39,46 @@ def _fill_row(row, value, ind, action = FILLED):
     return row
 
 
-def row_variation(row, blocks):
-    option = creating_base_option(row)
-    option_lst = []
-    _row_variation_helper(row, blocks, 0, option_lst, option)
+def _can_fill_variation(row, value, index):
+    return 0 not in row[index:index+value]
 
-def _row_variation_helper(row, blocks, ind, option_lst, option):
-    # base rule
-    if ind == len(row):
-        if option.count(FILLED):
-            return option_lst
-        option_lst.append(option[:])
-        return option_lst
-    # no more restrictions - everything filled
-    if blocks == []:
-        option[ind] = EMPTY
-        _row_variation_helper(row, blocks, ind + 1, option_lst, option)
-        option[ind] = UNKNOWN
-        return option_lst
-
-    if row[ind] == EMPTY:
-        _row_variation_helper(row, blocks, ind + 1, option_lst, option)
-
-    if row[ind] == FILLED:
-        return filled_value(blocks, ind, option_lst, option, row)
-
-    if row[ind] == UNKNOWN:
-        if ind + blocks[0] == len(row):
-            return filled_value(blocks, ind, option_lst, option, row)
-        if ind + blocks[0] > len(row):
-            return empty_value(blocks, ind, option_lst, option, row)
-
-        # will the restriction fit in this place
-        for i in range(ind, ind + blocks[0]):
-            if row[i] == EMPTY:
-                return empty_value(blocks, ind, option_lst, option, row)
-        if row[ind + blocks[0]] == FILLED or option.count(FILLED) + blocks[0] > sum(blocks):
-            return empty_value(blocks, ind, option_lst, option, row)
-        if row[ind + blocks[0]] == EMPTY:
-            return filled_value(blocks, ind, option_lst, option, row)
-
-        # couple of options
-        for value in [FILLED, EMPTY]:
-            if value == FILLED:
-                for num in range(ind, ind + blocks[0]):
-                    option[num] = value
-                option[ind + blocks[0]] = EMPTY
-                _row_variation_helper(row, blocks[1::], ind + blocks[0] + 1, option_lst, option)
-                option[ind] = UNKNOWN
-
-            else:
-                option[ind] = value
-                _row_variation_helper(row, blocks, ind + 1, option_lst, option)
-                option[ind] = UNKNOWN
-        return option_lst
-
-def creating_base_option(row):
-    option = []
-    for index in range(len(row)):
-        if row[index] != -1:
-            option[index] = row[index]
-    return option
-
-def filled_value(blocks, ind, option_lst, option, row):
-    option[ind] = FILLED
-    blocks[0] = blocks[0] - 1
-    unnecessary_restriction(blocks)
-    _row_variation_helper(row, blocks, ind + 1, option_lst, option)
-    return option_lst
-
-
-def empty_value(blocks, ind, option_lst, option, row):
-    option[ind] = EMPTY
-    _row_variation_helper(row, blocks, ind + 1, option_lst, option)
-    return option_lst
-
-
-def unnecessary_restriction(blocks):
-    if blocks[0] == 0:
-        blocks.remove(blocks[0])
-
-
-print(_row_variation_helper([0, -1, -1, -1, 1, -1, -1, 1, 0, 0, -1, 1], [2, 3, 2], 0, [], [""] * 12))
 
 def _check_sum(row):
     sum = 0
     for i in row:
-        if i == 1:
-            sum += 1
+        sum += i
     return sum
 
+
+def change_unknown_to_empty(row):
+    for val in range(len(row)):
+        if row[val] == UNKNOWN:
+            row[val] = EMPTY
+
+
 def row_variations(row, blocks):
-    pass
+    sum = _check_sum(blocks)
+    options = _row_variations_helper(row, blocks, 0, sum, [])
+    return options
 
-def _row_variations_helper(row, blocks, ind, sum ,lst):
-    if _check_sum(row) == sum:
-        lst.append(row[:])
+
+def _row_variations_helper(row, blocks, ind, sum, lst):
+    if not blocks:
+        temp_row = copy.deepcopy(row)
+        change_unknown_to_empty(temp_row)
+        if _check_sum(temp_row) == sum:
+            lst.append(temp_row)
         return row
-
     for index in range(ind, len(row)):
-        for value in range(blocks[0]):
-            if row[index] == UNKNOWN:
-                row[index] = FILLED
-                _row_variations_helper(row, blocks, index, sum, lst)
-                row[index] = UNKNOWN
-            if row[index] == EMPTY:
-                return
-            if row[index] == FILLED:
-                _row_variations_helper(row, blocks, index, sum - 1, lst)
-
-"""print(row_variations([1, 1, -1, 0], [3]))
-print(row_variations([-1, -1, -1, 0], [2]))
-print(row_variations([-1,0,1,0,-1],[1,1]))"""
-
+        if row[index] == UNKNOWN and _can_fill_variation(row, blocks[0], index):
+            _fill_row(row, blocks[0], index)
+            _row_variations_helper(row, blocks[1:], index + blocks[0] + 1, sum, lst)
+            _fill_row(row, blocks[0], index, UNKNOWN)
+        if row[index] == EMPTY:
+            continue
+        if row[index] == FILLED and _can_fill_variation(row, blocks[0], index):
+            _fill_row(row, blocks[0] - 1, index + 1)
+            _row_variations_helper(row, blocks[1:], index + 1, sum, lst)
+            _fill_row(row, blocks[0] - 1, index + 1, UNKNOWN)
+    return lst
 
--- test_nonogram.py
+++ test_nonogram.py
@@ -7,3 +7,22 @@ def test_can_fill():
     assert nonogram._can_fill([1, 1, 1, 0, 0], 1, 4) == True
     assert nonogram._can_fill([1,1,1,1,0], 2, 0) == False
     assert nonogram._can_fill([1, 1, 0, 0, 0], 1, 2) == False
+
+def test_constraint():
+    assert nonogram.constraint_satisfactions(3, [1]) == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    assert nonogram.constraint_satisfactions(3, [2]) == [[1, 1, 0], [0, 1, 1]]
+    assert nonogram.constraint_satisfactions(3, [1,1]) == [[1, 0, 1]]
+    assert nonogram.constraint_satisfactions(4, [1,1]) == [[1, 0, 1, 0], [1, 0, 0, 1], [0, 1, 0, 1]]
+    assert nonogram.constraint_satisfactions(5, [2,1]) == [[1, 1, 0, 1, 0], [1, 1, 0, 0, 1], [0, 1, 1, 0, 1]]
+    assert nonogram.constraint_satisfactions(2,[3]) == []
+
+def test_row_variation():
+    assert nonogram.row_variations([1, 1, -1, 0], [3]) == [[1, 1, 1, 0]]
+    assert nonogram.row_variations([-1, -1, -1, 0], [2]) == [[1, 1, 0, 0], [0, 1, 1, 0]]
+    assert nonogram.row_variations([-1, 0, 1, 0, -1, 0], [1, 1]) == [[1, 0, 1, 0, 0, 0], [0, 0, 1, 0, 1, 0]]
+    assert nonogram.row_variations([-1, -1, -1], [1]) == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    assert nonogram.row_variations([0, 0, 0], [1]) == []
+    assert nonogram.row_variations([0, 0, -1, 1, 0], [3]) == []
+    assert nonogram.row_variations([0, 0, -1, 1, 0], [2]) == [[0, 0, 1, 1, 0]]
+    assert nonogram.row_variations([0, 0, 1, 1, 0], [2]) == [[0, 0, 1, 1, 0]]
+    assert nonogram.row_variations([0, 0, -1, -1], [5]) == []
