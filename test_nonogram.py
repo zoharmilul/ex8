df012efa5bdfe3d11b1e5e5f8d39b20f355e6735
zoharmilul <8qxddflhxbw>
2020-12-15 16:02:15 +0200

    new

--- test_nonogram.py
+++ test_nonogram.py
@@ -15,7 +15,7 @@ def test_constraint():
     assert nonogram.constraint_satisfactions(4, [1,1]) == [[1, 0, 1, 0], [1, 0, 0, 1], [0, 1, 0, 1]]
     assert nonogram.constraint_satisfactions(5, [2,1]) == [[1, 1, 0, 1, 0], [1, 1, 0, 0, 1], [0, 1, 1, 0, 1]]
     assert nonogram.constraint_satisfactions(2,[3]) == []
-
+    assert nonogram.constraint_satisfactions(2,[]) == [[0, 0]]
 def test_row_variation():
     assert nonogram.row_variations([1, 1, -1, 0], [3]) == [[1, 1, 1, 0]]
     assert nonogram.row_variations([-1, -1, -1, 0], [2]) == [[1, 1, 0, 0], [0, 1, 1, 0]]
